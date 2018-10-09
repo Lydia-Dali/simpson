925ec8d1a0323b2eb02b10fa19de2dc350dad4fd
lydia.dal@live.fr <lydia.dal@live.fr>
2018-10-09 16:08:07 +0200

    Lamp.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
+// src/App.js
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import Quote from "./Quote";
+// import Quote from "./Quote";
+import Quotes from "./Quotes";
+import Lamp from "./Lamp";
 
 class App extends Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    working: true
+    };
+  }
+  handleClick = () => {
+    this.setState({ working: !this.state.working });
+  } 
+
   render() {
+    const bart = this.state.working ? 'On' : 'Off';
+
+  let changeLogo= 'App-logo2';
+  if (this.state.working){
+    changeLogo = 'App-logo';
+  }
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Simpsons Quotes</h1>
+          
+          <img src={logo} className={changeLogo} alt="logo" />
+          {/* <h1 className="App-title">Simpsons Quotes</h1> */}
+
+          <button
+          className={bart}
+          onClick={
+            this.handleClick
+          }>
+          
+          {bart.toUpperCase()}
+        </button>
+        <h1>Simpsons Quotes</h1>
         </header>
-        <Quote
-          quote="I believe the children are the future... Unless we stop them now!"
-          character="Homer Simpson"
-          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
-        />
-        <Quote
-          quote="Me fail English? That's unpossible"
-          character="Ralph Wiggum"
-          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
-        />
+        
+        <Lamp on />
+        <Lamp />
+        <Quotes />
+        
       </div>
     );
   }
